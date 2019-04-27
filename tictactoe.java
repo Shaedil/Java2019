455361ef0f7677ea7560fe0dafe780136a6f0bb7
Shaedil <shaedildider@gmail.com>
2019-04-26 21:09:47 -0400

    init tictactoe.java

---
+++ tictactoe.java
@@ -0,0 +1,12 @@
+public class tictactoe.java {
+    public static void main(String[] args) {
+        start();
+    }
+    public static void start() {
+        System.out.println("====TicTacToe===="); 
+    }
+    public static void niggers() {
+        
+    }
+
+}
