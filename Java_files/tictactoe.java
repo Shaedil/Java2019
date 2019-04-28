05b599504032fdc6088824a9278ce65c9d799db1
Shaedil <shaedildider@gmail.com>
2019-04-28 11:47:51 -0400

    make tictactoe able to run

--- Java_files/tictactoe.java
+++ Java_files/tictactoe.java
@@ -1,11 +1,11 @@
-// A program that prints an ascii tictactoe board and allows you to play with another person. AI not supported.
-// TODO: Figure out a way to 
+import java.util.Scanner;
 
 public class tictactoe {
     // Creates a 2d array 'coordinate like board'
-    private static char[][] board; 
+    private static char[][] board;
     private static char currentPlayerMark;
-
+    private static int row;
+    private static int col;
     public static void TicTacToe() {
         // Defines the board to be 3x3 in size
         board = new char[3][3];
@@ -34,21 +34,99 @@ public class tictactoe {
         }
     }
     // Switch player turns
-    public static void playerTurn() {
+    public static char changePlayer() {
         if (currentPlayerMark == 'X') {
-            currentPlayerMark = 'O';    
+            currentPlayerMark = 'O';
+            return 'X';
         }
         else {
             currentPlayerMark = 'X';
+            return 'O';
         }
     }
     // Force player to make a move
-    public static void playerMove() {
-            
+    public static boolean placeMark(int rows, int cols) {
+        if ((row >= 0) && (col < 3)) {
+            if ((col >= 0) && (col < 3)) {
+                if (board[row][col] == ' ') {
+                    board[row][col] = currentPlayerMark;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean isBoardFull() {
+        boolean isFull = true;
+        for(int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    isFull = false;
+                }
+            }
+        }
+        return isFull;
+    }
+    public static boolean checkForWin() {
+        return (checkRows() || checkCols() || checkDiags());
+    }
+    // Check wins
+    public static boolean checkRows() {
+        for(int i = 0; i < 3; i++) {
+            if (checkRowCol(board[i][0], board[i][1], board[i][2]) == true) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean checkCols() {
+        for (int i = 0; i < 3; i++) {
+            if (checkRowCol(board[0][i], board[1][i], board[2][i]) == true) {
+                return true;    
+            }
+        }
+        return false;
+    }
+    public static boolean checkDiags() {
+        return ((checkRowCol(board[0][0], board[1][1], board[2][2]) == true) || (checkRowCol(board[0][2], board[1][1], board[2][0]) == true));
+    }
+    public static boolean checkRowCol(char c1, char c2, char c3) {
+        return ((c1 != ' ') && (c1 == c2) && (c2 == c3));    
+    }
+    public static void printInfo() {
+        System.out.println("+==================+TicTacToe+==================+");
+        System.out.println("     The board is ordered like a coordinate" + "\n");
+        System.out.println("               system. See this:           " + "\n");
+        System.out.println("                 |0,0|1,0|2,0|             " + "\n");
+        System.out.println("                 |0,1|1,1|2,1|             " + "\n");
+        System.out.println("                 |0,2|1,2|2,2|             " + "\n");
+        System.out.println("          +_________________________+" + "\n");
+    }
+    public static void runner() {
+        Scanner reader = new Scanner(System.in);
+        while (checkForWin() == false) {
+            if (changePlayer() == 'X') {
+                System.out.println("Type in the X coordinate: ");
+                int x = reader.nextInt();
+                System.out.println("Type in the Y coordinate: ");
+                int y = reader.nextInt();
+                placeMark(x,y);
+                printBoard();
+            }
+            else {
+                System.out.println("Type in the X coordinate: ");
+                int x = reader.nextInt();
+                System.out.println("Type in the Y coordinate: ");
+                int y = reader.nextInt();
+                placeMark(x,y);
+                printBoard();
+            }
+        }
     }
     public static void main(String[] args) {
-         TicTacToe();
-         printBoard();
-        
+        printInfo();
+        TicTacToe();
+        printBoard();
+        runner();        
     }
 }
